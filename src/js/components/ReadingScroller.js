ce0707af9bf963d00943509bdd699e6276019a6c
yuta <bugs_bugs_bunny69@yahoo.co.jp>
2017-02-08 14:17:27 +0900

    [WIP]reading scroller

--- src/js/components/ReadingScroller.js
+++ src/js/components/ReadingScroller.js
@@ -1,21 +1,43 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { readScroll } from '../actions'
 
-const ReadingScroller = (state) => {
-  let scrollTop = 0
-  // window.addEventListener('scroll', function (e) {
-  //   e.preventDefault()
-  //   scrollTop = window.pageYOffset
-  //   state.onReadingScroller(scrollTop)
-  // })
-  // window.addEventListener('resize', function (e) {
-  //   e.preventDefault()
-  //   console.log('resize')
-  // })
-  return (
-    <section className={'js-target'}>
-      <p>ReadingScroller: {state.scrollY}</p>
-    </section>
-  )
+// const ReadingScroller = (state) => {
+//   let scrollTop = 0
+//   console.log(this)
+//   window.addEventListener('scroll', function (e) {
+//     e.preventDefault()
+//     scrollTop = window.pageYOffset
+//     state.onReadingScroller(scrollTop)
+//   })
+//   window.addEventListener('resize', function (e) {
+//     e.preventDefault()
+//     console.log('resize')
+//   })
+//   return (
+//     <section className={'js-target'}>
+//       <p>ReadingScroller: {state.scrollY}</p>
+//     </section>
+//   )
+// }
+// export default ReadingScroller
+
+class ReadingScroller extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      scrollY: this.props.scrollY
+    }
+  }
+  componentDidMount() {
+    console.log(this.props)
+    window.addEventListener('scroll', (e) => {
+      e.preventDefault()
+      this.state.scrollY = window.pageYOffset
+    })
+  }
+  render() {
+    return <div>{this.state.scrollY}</div>;
+  }
 }
+
 export default ReadingScroller
